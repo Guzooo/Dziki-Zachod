83d4b103b0d4b7f9d870edbacdf0430cdfa29d4c
Guzooo <guzooo-guzooo@o2.pl>
2018-06-10 14:39:42 +0200

    0.0.4
    Fragment społeczność wczytuje stronę i pozwala cofać przeglądane strony

--- java/com/Guzooo/DzikiZachod2017/SpolecznoscFragment.java
+++ java/com/Guzooo/DzikiZachod2017/SpolecznoscFragment.java
@@ -2,6 +2,7 @@ package com.Guzooo.DzikiZachod2017;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,6 @@ import android.webkit.WebViewClient;
 
 
 public class SpolecznoscFragment extends Fragment {
-
 
     public SpolecznoscFragment() {
         // Required empty public constructor
@@ -21,10 +21,29 @@ public class SpolecznoscFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View layout = inflater.inflate(R.layout.fragment_spolecznosc, container, false);
-
         WebView webView = layout.findViewById(R.id.instagram);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.google.pl");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.instagram.com/explore/tags/kurozw%C4%99ki/");
+
+        webView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction() == KeyEvent.ACTION_DOWN) {
+                    WebView webView = (WebView) v;
+
+                    switch(keyCode) {
+                        case KeyEvent.KEYCODE_BACK:
+                            if(webView.canGoBack()) {
+                                webView.goBack();
+                                return true;
+                            }
+                            break;
+                    }
+                }
+                return false;
+            }
+        });
 
         return layout;
     }
